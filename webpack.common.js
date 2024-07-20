034029270c54727dca89429b85790a7926213d60
Muzaffer <muzzusyed153@gmail.com>
2024-07-20 21:58:06 +0530

    Add babe config file and babel package

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { optimize } = require("webpack");
 
 module.exports = {
   entry: {
-    main: './src/main.js',
+    main: "./src/main.js",
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
 
       {
         test: /\.(jpg|png|svg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: './images/[name][ext]',
+          filename: "./images/[name][ext]",
         },
       },
       {
         test: /\.(woff|woff2)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: './fonts/[name][ext]',
+          filename: "./fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
     ],
@@ -32,10 +43,10 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'template',
-      filename: 'index.html',
-      template: './src/index.html',
-      eject: 'body',
+      title: "template",
+      filename: "index.html",
+      template: "./src/index.html",
+      eject: "body",
     }),
   ],
 };
